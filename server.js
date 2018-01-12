fbeb019a96290e86c5f42462427a03e5472093e8
Sierra <chris.w894j@gmail.com>
2018-01-12 17:46:27 +0900

    Setup start script and heroku port

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require ('express');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+
+app.set('view engine', 'hbs');
+//app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {return new Date().getFullYear()});
+hbs.registerHelper('screamIt', (text) => {return text.toUpperCase()});
+
+
+app.get('/about', (req, res) => {
+		res.render('about.hbs', {
+pageTitle:'Home Page',
+welcomeMessage: 'Welcome to my home page!',
+
+		});
+})
+
+
+app.get('/', (req, res) => {
+		res.render('home.hbs', {
+pageTitle:'Home Page',
+welcomeMessage: 'Welcome to my home page!',
+		});
+})
+
+
+
+app.get('/bad', (req, res) => {
+	res.send({errorMessage: 'uNable to handle request'
+		});
+});
+
+app.listen(port, () =>
+{
+console.log(`Server is up at ${port}`);
+})
